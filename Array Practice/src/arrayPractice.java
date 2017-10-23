bf6fbc1daa8c4b5262a179b7400f2042e10a72aa
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-23 13:14:38 -0400

    Array Practice(10/23/17)

--- Array Practice/src/arrayPractice.java
+++ Array Practice/src/arrayPractice.java
@@ -3,17 +3,35 @@ public class arrayPractice {
 	
 	public static void main(String[] args)
 	{
-		String foo = "foo";
+		/*String foo = "foo";
 		String foofoo = doubled(foo);
 		
 		System.out.println(foo);
-		System.out.println(foofoo);
+		System.out.println(foofoo); */
+		
+		String[] flavors = new String[2];
+		flavors[0] = "chocolate";
+		flavors[1] = "vanilla";
+		
+		doubledArray(flavors);
+		
+		System.out.println(flavors[0]);
+	
 	}
 	
 	public static String doubled(String str1)
 	{
 		str1 = str1 + str1;
 		return str1;
+	}
+	
+	public static String[] doubledArray(String[] arr1)
+	{
+		for(int x = 0;x < arr1.length;x++)
+		{
+			arr1[x] = arr1[x] + arr1[x];
+		}
+		return arr1;
 	}
 }
 
