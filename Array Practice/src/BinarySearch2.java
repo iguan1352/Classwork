de6b28fd0bb60fcfc1f69865e79e1bdec18612b7
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-15 12:08:51 -0500

    11/15/17

--- Array Practice/src/BinarySearch2.java
+++ Array Practice/src/BinarySearch2.java
@@ -1,21 +1,26 @@
 
-public class binarySearch {
+public class BinarySearch2 {
 
 	public static void main(String[] args)
 	{	
-		int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-		int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
+		int[] test1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int[] test2 = {1, 2, 3, 4, 5, 6, 7, 8};
 		
-		System.out.println(BinarySearch(test1, 3, 0, test1.length)); //2
-		System.out.println(BinarySearch(test2, 23, 0, test2.length)); //7
-		System.out.println(BinarySearch(test1, 1, 0, test1.length)); //0
-		System.out.println(BinarySearch(test2, 25, 0, test2.length)); //-1 
-		
+		for(int i = -1; i < test1.length+1; i++)
+		{
+			System.out.println(BinarySearch(test1, i, 0, test1.length));
+		}
+		System.out.println();
+		for(int i = -1; i < test2.length+1; i++)
+		{
+			System.out.println(BinarySearch(test2, i, 0, test2.length));
+		}
+		System.out.println();
 	}
 
 	public static int BinarySearch(int[] nums, int query, int first, int last)
 	{
-		if(last >= first)
+		if(last > first)
 		{
 			int index = (first+last)/2;
 			int guess = nums[index];
